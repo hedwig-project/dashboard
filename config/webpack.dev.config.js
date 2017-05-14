60a5bd06d3948cd7eefc48a88cd146b216cf3772
Daniela Sayuri Yassuda <danielayassuda@gmail.com>
2017-05-14 16:11:26 -0300

    Add React, Redux and React Router dependencies, configure webpack

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,85 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    bundle: [
+      'react-hot-loader/patch',
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      'babel-polyfill',
+      path.resolve(__dirname, '../src/index.js'),
+    ],
+  },
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].[hash].js',
+    publicPath: '/',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    contentBase: path.resolve(__dirname, '../dist'),
+    port: 3000,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+        exclude: /flexboxgrid/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+        include: /flexboxgrid/,
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 25000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src/index.html'),
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /pt-br/),
+  ],
+};
