500399c9da2cd86d7f1a79d704dfe1391d070658
Gabriela Melo <gabimelo10@gmail.com>
2017-07-19 19:27:00 -0300

    Pushing files that accidentally weren\'t added to last commit

---
+++ src/routes/Login/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Login from '@routes/Login/containers/Login'
+import GoToSignup from '@routes/Login/containers/GoToSignUp'
+
+export default () => (
+  <div className="central-container">
+    <Login />
+    <GoToSignup />
+  </div>
+)
