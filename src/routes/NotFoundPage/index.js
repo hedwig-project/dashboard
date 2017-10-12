d293c5a2b3fc492ecbdd21a19701e05bb496a8db
Daniela Sayuri Yassuda <danielayassuda@gmail.com>
2017-10-12 18:51:32 -0300

    Style 404 page

--- src/routes/NotFoundPage/index.js
+++ src/routes/NotFoundPage/index.js
@@ -1,7 +1,30 @@
 import React, { PropTypes } from 'react'
+import styled from 'styled-components'
+import pattern from '@images/au.jpg'
+
+const Wrapper = styled.section`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to bottom, rgba(255,255,255,1) 0%, rgba(255,255,255,0.5) 80%, rgba(0,0,0,0) 100%), url(${pattern});
+  color: #424242;
+`
+
+const Title = styled.h1`
+  font-size: 128px;
+`
+
+const Text = styled.div`
+  font-size: 24px;
+  padding: 20px 0;
+`
 
 export default () => (
-  <div>
-    <p>404</p>
-  </div>
+  <Wrapper>
+    <Title>404</Title>
+    <Text>Ooops! Parece que essa página não existe :(</Text>
+  </Wrapper>
 )
